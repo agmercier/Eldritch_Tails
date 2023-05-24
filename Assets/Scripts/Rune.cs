79653b0af58cdc8df3eb3391b766c4d11c569968
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2023-05-24 03:37:55 +0200

    Add runes

---
+++ Assets/Scripts/Rune.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Rune : MonoBehaviour
+{
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void ChangeWhenHover()
+    {
+        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 0.5f);
+    }
+
+    public void ChangeWhenLeaves()
+    {
+        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 0.0f);
+    }
+}
